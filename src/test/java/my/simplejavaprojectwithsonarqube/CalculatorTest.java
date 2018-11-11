11c7900fba827d61b5ed6eee6294bf76ce64f9e5
pipelineascodecourse <committer@example.com>
2018-11-11 23:39:56 +0800

    initial commit

--- src/test/java/my/simplejavaprojectwithsonarqube/CalculatorTest.java
+++ src/test/java/my/simplejavaprojectwithsonarqube/CalculatorTest.java
@@ -14,7 +14,7 @@ public class CalculatorTest {
 	
 		Calculator calculator = new Calculator();
 		int result = calculator.add(1, 2);
-		assertEquals(3, result);
+		assertEquals(4, result);
 		
 	}
 	
