7d45f8217bdc36b568517dab4b57ca4e27ffce68
ramsabari97 <139346554+ramsabari97@users.noreply.github.com>
2023-07-27 15:56:07 +0530

    Update Calculator.java

--- src/main/java/my/simplejavaprojectwithsonarqube/Calculator.java
+++ src/main/java/my/simplejavaprojectwithsonarqube/Calculator.java
@@ -4,7 +4,7 @@ public class Calculator {
 
 	public int add(int a, int b)
 	{
-		// System.out.println("bug on purpose:"+(a=+b));
+		System.out.println("bug on purpose:"+(a=+b));
 		return a+b;
 	}
 
@@ -22,4 +22,12 @@ public class Calculator {
 	{
 		return a*b;
 	}	
+
+	public int getRandomNumber()
+	{
+		System.out.println("Vulnerability on purpose");
+		SecureRandom sr = new SecureRandom();
+		sr.setSeed(123456L);
+		return sr.nextInt();
+	}
 }
